6c54f1652e1f07233edeed6cf4d84995e1282942
xiaohan <xiaohan2012@gmail.com>
2013-10-24 10:17:33 +0300

    crawl question for hxiao and nobody done\!

--- crawljson.sh
+++ crawljson.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 NOW=$(date +"%F--%T")
+cookiejar="cookies/hxiao.cjar"
+url="http://stackoverflow.com"
 
-./crawl.sh | ./page2json > "data/hxiao/$NOW.json"
+userpage=$(curl -b $cookiejar -c $cookiejar $url)
+nobodypage=$(curl $url)
 
-curl http://stackoverflow.com | ./page2json > "data/nobody/$NOW.json"
+if [  "$1" == "test" ]; then
+    echo "test tag python count"
+    echo $userpage | ./page2json |  ./counttag python
+    echo $nobodypage | ./page2json | ./counttag python
+else
+    echo $userpage | ./page2json > "data/hxiao/$NOW.json"
+    echo $nobodypage | ./page2json > "data/nobody/$NOW.json"
+fi
+
 
